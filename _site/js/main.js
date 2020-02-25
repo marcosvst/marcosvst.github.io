cf76e75dd03bc6addaebfaa8da0fca6381c2d8c3
Marcos Vinícius <marcosvst98@gmail.com>
2020-02-25 04:29:36 -0300

    primeiro commit

---
+++ _site/js/main.js
@@ -0,0 +1,31 @@
+$(document).on("scroll",function(){
+
+    // Scrolling effect
+    if($(document).scrollTop() > 300){ 
+        $("nav").addClass("bg-light-custom");
+        $("nav").removeClass("navbar-toggler-icon");
+        $("span").addClass("navbar-toggler-icon-custom");
+    } else{
+        $("nav").removeClass("bg-light-custom");
+        $("span").removeClass("navbar-toggler-icon-custom");
+    }
+
+});
+
+// (function() {
+//     function scrollHorizontally(e) {
+//         e = window.event || e;
+//         var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+//         document.getElementById('scroll-overflow').scrollLeft -= (delta*40); // Multiplied by 40
+//         e.preventDefault();
+//     }
+//     if (document.getElementById('scroll-overflow').addEventListener) {
+//         // IE9, Chrome, Safari, Opera
+//         document.getElementById('scroll-overflow').addEventListener("mousewheel", scrollHorizontally, false);
+//         // Firefox
+//         document.getElementById('scroll-overflow').addEventListener("DOMMouseScroll", scrollHorizontally, false);
+//     } else {
+//         // IE 6/7/8
+//         document.getElementById('scroll-overflow').attachEvent("onmousewheel", scrollHorizontally);
+//     }
+// })();
